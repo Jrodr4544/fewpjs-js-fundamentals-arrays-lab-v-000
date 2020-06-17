1afbdf595fd10bd6af30e5c404de77be39561dc0
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-17 02:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-const arr = ["Milo", "Otis", "Garfield"];
+const append = ["Milo", "Otis", "Garfield"];
 const append = arr.push('Odie');
 const prepend = arr.unshift('Odie');
 const removeLast = arr.pop();
