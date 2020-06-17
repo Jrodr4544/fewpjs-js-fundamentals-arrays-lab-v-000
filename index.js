3264258e4e0eb9cad1a329a6cb9b1d1b3edd96e9
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-17 02:18:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const arr = ["Milo", "Otis", "Garfield"];
 const append = arr.push('Odie');
 const prepend = arr.unshift('Odie');
 const removeLast = arr.pop();
-const removeFirst = arr;
+const removeFirst = arr.shift();
 
