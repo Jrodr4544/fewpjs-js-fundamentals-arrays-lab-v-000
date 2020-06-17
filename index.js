32b23c04eb35453ac5f45b8fa40b2656e439723c
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-17 02:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution here!
 const arr = ["Milo", "Otis", "Garfield"];
 const append = arr.push('Odie');
-const prepend = arr.unshift('');
+const prepend = arr.unshift('Odie');
 const removeLast = arr.unshift();
 const removeFirst = arr;
 
